a89fadd113adbf9b9433fc25669ed911833c3ce8
Aliaksei Iuko <119662075+Aliaksei42@users.noreply.github.com>
2024-01-19 11:36:05 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ const URL =
   'mongodb+srv://alex:Passs321@cluster0.tq2r9lo.mongodb.net/autoSite?retryWrites=true&w=majority'
 // const URL = 'mongodb://localhost:27017/autoSite'
 
-const corsOptions = {
-  origin: 'https://plankton-app-crfpr.ondigitalocean.app/',
-  optionsSuccessStatus: 200,
-  credentials: true,
-}
+//const corsOptions = {
+//  origin: 'https://plankton-app-crfpr.ondigitalocean.app/',
+//  optionsSuccessStatus: 200,
+//  credentials: true,
+// }
 
 const app = express()
 app.use(cors(corsOptions))
